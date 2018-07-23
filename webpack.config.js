6767c0b31c4d4d9207dde2094b55802ddbcb97d9
YuriNem <PsychoactivePie@gmail.com>
2018-07-23 23:52:40 +0300

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,6 @@
-const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
